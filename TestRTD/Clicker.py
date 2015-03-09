930f76284e411068fffc80a05f13707aec769cad
dstrohl <Kur1tacams>
2015-03-08 17:14:43 -0700

    initial commit

--- TestRTD/Clicker.py
+++ TestRTD/Clicker.py
@@ -6,6 +6,7 @@ import sys
 # counter Class
 # ===============================================================================
 
+__all__ = ['Clicker']
 
 class ClickItem(object):
 
--- TestRTD/docs/index.rst
+++ TestRTD/docs/index.rst
@@ -14,6 +14,7 @@ Contents:
    :maxdepth: 2
 
 Test
+
 .. automodule:: TestRTD.Clicker
     :members:
 
